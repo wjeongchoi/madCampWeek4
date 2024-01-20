52bbbe920678f5b7e79dfe6e7cb50896f3bcc6c4
wjeongchoi <artificialmint@gmail.com>
2024-01-21 04:31:21 +0900

    :sparkles: save warning

--- back/app.js
+++ back/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usageRouter = require('./routes/usage');
+var warningRouter = require('./routes/warning');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/usage',usageRouter);
+app.use('/warning', warningRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
