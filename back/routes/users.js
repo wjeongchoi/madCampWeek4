3290a17c92bfce4acb998c44f3bcffaa1c1d3c7b
wjeongchoi <artificialmint@gmail.com>
2024-01-21 04:48:39 +0900

    :sparkles: user delete api

--- back/routes/users.js
+++ back/routes/users.js
@@ -70,5 +70,60 @@ router.post("/login", function (req, res) {
   });
 });
 
+// 사용자 정보 삭제 API
+router.post('/delete', function(req, res) {
+  const { id, password } = req.body;
+
+  // 먼저 사용자의 현재 비밀번호 확인
+  const userQuery = 'SELECT password FROM Users WHERE id = ?';
+  database.query(userQuery, [id], async (error, results, fields) => {
+    if (error) {
+      return res.status(500).send('Error in fetching user');
+    }
+
+    if (results.length > 0) {
+      // 비밀번호 비교
+      const match = await bcrypt.compare(password, results[0].password);
+      if (match) {
+        // ProgramUsage 테이블에서 관련 데이터 삭제
+        const deleteProgramUsageQuery = 'DELETE FROM ProgramUsage WHERE user_id = ?';
+        database.query(deleteProgramUsageQuery, [id], (deleteError, deleteResults, deleteFields) => {
+          if (deleteError) {
+            return res.status(500).send('Error in deleting program usage data');
+          }
+
+          // Warnings 테이블에서 관련 데이터 삭제
+          const deleteWarningsQuery = 'DELETE FROM Warnings WHERE user_id = ?';
+          database.query(deleteWarningsQuery, [id], (warningError, warningResults, warningFields) => {
+            if (warningError) {
+              return res.status(500).send('Error in deleting warnings data');
+            }
+
+            // UserSettings 테이블에서 관련 데이터 삭제
+            const deleteUserSettingsQuery = 'DELETE FROM UserSettings WHERE user_id = ?';
+            database.query(deleteUserSettingsQuery, [id], (settingsError, settingsResults, settingsFields) => {
+              if (settingsError) {
+                return res.status(500).send('Error in deleting user settings data');
+              }
+
+              // 사용자 삭제
+              const deleteUserQuery = 'DELETE FROM Users WHERE id = ?';
+              database.query(deleteUserQuery, [id], (deleteUserError, deleteUserResults, deleteUserFields) => {
+                if (deleteUserError) {
+                  return res.status(500).send('Error in deleting user');
+                }
+                res.status(200).send('User deleted successfully');
+              });
+            });
+          });
+        });
+      } else {
+        res.status(401).send('Password does not match');
+      }
+    } else {
+      res.status(404).send('User not found');
+    }
+  });
+});
 
 module.exports = router;
